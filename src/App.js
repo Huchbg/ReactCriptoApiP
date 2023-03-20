2eeaa441fdfb176c11c19e84d7aff5f115a5a82b
Huchbg <sklaqr@gmail.com>
2023-03-20 20:51:44 +0200

    Innitial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import  styled  from "styled-components"
+
+
+function App() {
+  return (
+    <Wraper className="App">
+      hello
+    </Wraper>
+  );
+}
+
+const Wraper=styled.div`
+  color: red ;
+`
+
+export default App;
